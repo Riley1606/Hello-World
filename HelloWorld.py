ee957d4c6d21f16b80f99d951838d5f3255a19c9
Riley1606 <riley.maurer@mnsu.edu>
2024-09-06 14:31:47 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+human_age = input("enter age: ")
+print(human_age)
+dog_age = int(human_age) * 7
+print(dog_age)
+cat_age = int(human_age) / 9
+print(cat_age)
+x = int(human_age)**2
+horse_age = 3*(((x-47)/7)+12)
+print(horse_age)
